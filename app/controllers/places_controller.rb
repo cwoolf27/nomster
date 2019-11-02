1963046ea1b84757be39cbebe62f2f86e856c03d
Cactus Woolf <cactuswoolf@gmail.com>
2019-11-02 14:38:45 +0000

    adding carousel slider

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,7 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.paginate(page: params[:page], per_page: 1)
+    @testimage = Place.first.photos.first
     #@places = Place.all
   end
 
